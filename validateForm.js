c9be3bcaecea6d1b4a4b0152b7de4ab110bba368
Vincent Ndokaj <vinndokaj@gmail.com>
2020-10-13 19:56:15 -0400

    test

--- validateForm.js
+++ validateForm.js
@@ -1,35 +1,33 @@
 function validateForm(){
-  var name = document.forms.personalInfo.name;
-  var gender = document.forms.personalInfo.gender;
-  var email = document.forms.personalInfo.email;
-  var iseasy = document.forms.personalInfo.iseasy;
-  var data;
+  var name = document.forms.personalInfo.name.value;
+  var gender = document.forms.personalInfo.gender.value;
+  var email = document.forms.personalInfo.email.value;
+  var iseasy = document.forms.personalInfo.iseasy.value;
+  var data = name + "," + gender + "," + email + "," + iseasy;;
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   //validate inputs before storing any data
-  if(name.value == ""){
+  if(name == ""){
     alert("Please fill out your name.");
     name.focus();
     return false;
   }
 
-  if (gender.value == ""){  //!(gender[0].checked || gender[1].checked || gender[2].checked)
+  if (gender == ""){
     alert("Please choose your gender.")
     return false;
   }
 
-  if(!email.value.match(mailformat)){
+  if(!email.match(mailformat)){
     alert("Please enter a valid email address.");
     email.focus();
     return false;
   }
 
-  if(iseasy.value == ""){  //!(iseasy[0].checked || iseasy[1].checked)
+  if(iseasy == ""){
     alert("Please choose whether or not it was easy.");
     return false;
   }
-  //begin to store data once validated
-  data = name.value + "," + gender.value + "," + email.value + "," + iseasy.value;
 
   //insert data into blob type for csv insertion
   const dataBlob = new Blob([data],{type:'text/csv'});
